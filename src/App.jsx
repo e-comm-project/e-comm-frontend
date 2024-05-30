dad125c65b0f7aaf94262af58097f207ac00229a
Richard Hadzhiev <ri4koto100@gmail.com>
2024-05-30 11:30:59 +0200

    Refactor App.jsx and main.jsx to add new pages and update dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import HomePage from "./pages/HomePage";
+import ProductDetails from "./pages/ProductDetails";
+import ProductList from "./pages/ProductList";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <HomePage />
+      <ProductDetails />
+      <ProductList />
+    </>
+  );
 }
 
 export default App;
